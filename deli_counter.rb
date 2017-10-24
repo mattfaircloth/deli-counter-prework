e534e5ea08856cf82e656b994053a6b59936d0cd
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-24 18:17:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,6 @@ def line(katz_deli)
     katz_deli.each_with_index.map do |name, index|
       deli_line.push("#{index + 1}. #{name}")
   end
+  puts "The line is currently: " + deli_line.join(", ")
  end
-   puts "The line is currently " + deli_line.join(", ")
 end
