c957dfc5c9bf6c367afcdb6c625c6ffcb7e7b63c
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-24 18:11:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     katz_deli.each_with_index.map do |name, index|
-      "Hello, #{name}. You are currently #{index + 1} in line."
+      puts "The line is currently #{index + 1}. #{name}"
   end
  end
 end 
